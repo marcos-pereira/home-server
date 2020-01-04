28dc513c29d833c4e9c07eb5b3e8a02832bb59f8
Marcos Pereira <marcos.si.pereira@gmail.com>
2020-01-04 16:06:08 -0300

    Create test_view1

--- sandbox/django_tutorial/mysite/polls/views.py
+++ sandbox/django_tutorial/mysite/polls/views.py
@@ -7,3 +7,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def test_view1(request):
+    return HttpResponse("Test view1")
