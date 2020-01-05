0231bb34545420089c13831270be80d30b87e48a
Marcos Pereira <marcos.si.pereira@gmail.com>
2020-01-04 21:24:22 -0300

    Update index view to use render

--- sandbox/django_tutorial/mysite/polls/views.py
+++ sandbox/django_tutorial/mysite/polls/views.py
@@ -4,8 +4,8 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def test_view1(request):
     return HttpResponse("Test view1")
