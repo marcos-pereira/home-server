219180dd591560a7304855c3141c4c46fc212a25
Marcos Pereira <marcos.si.pereira@gmail.com>
2020-01-04 21:11:30 -0300

    Add detail, results and vote views

--- sandbox/django_tutorial/mysite/polls/views.py
+++ sandbox/django_tutorial/mysite/polls/views.py
@@ -10,3 +10,13 @@ def index(request):
 
 def test_view1(request):
     return HttpResponse("Test view1")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
