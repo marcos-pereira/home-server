5a2ef04a5e1be40f5481b4de928cc80c064eff90
Marcos Pereira <marcos.si.pereira@gmail.com>
2020-01-04 21:19:23 -0300

    Modify index view to show the last 5 questions

--- sandbox/django_tutorial/mysite/polls/views.py
+++ sandbox/django_tutorial/mysite/polls/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def test_view1(request):
     return HttpResponse("Test view1")
