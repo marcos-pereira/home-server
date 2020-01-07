02f2a0c07e520ab63e25eb043699452a7a9e5cb2
Marcos Pereira <marcos.si.pereira@gmail.com>
2020-01-07 20:04:15 -0300

    Render Post model

--- sandbox/blog_tutorial/django_blog_tutorial/blog/views.py
+++ sandbox/blog_tutorial/django_blog_tutorial/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'MarcosSP',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 6, 2020'
-    },
-    {
-        'author': 'JuniorLP',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 7, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
